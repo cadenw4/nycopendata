daac3e60f05615b90e4fac3b9fe1a906d01e6924
cadenw4 <cadenw4@nycstudents.net>
2026-05-29 13:10:02 +0000

    filter page WIP

--- script.js
+++ script.js
@@ -5,11 +5,37 @@ async function init(){
   data = await info.json();
 }
 
-function display(){
+/*function display(){
     build = ""
     output = document.getElementById("output")
+    result = document.getElementById("result")
     for(let i = 0; i<data.length; i += 1){
       sq = data[i]
-    build += `<div class = "fitted card">`
+    build += `<div class = "fitted card">
+    <h2>${sq.x}</h2>`
     }
+    output.innerHTML = build;
+    result.innerHTML = ""
+}
+*/
+
+function reset(){
+  let output = document.getElementById("output");
+  let result = document.getElementById("result");
+  let build = "";
+for(let i = 0; i < data.length; i+=1){
+    let sq = data[i];
+if(sq.primary_fur_color == null){
+  sq.primary_fur_color = ""
+}
+    build += `<div class="fitted card">
+  <h1>ID: ${sq.unique_squirrel_id} </h1> 
+  <p>Coordinates: ${sq.x}, ${sq.y}</p>
+  <p>Fur color: </p> <p>${sq.primary_fur_color}</p>
+              </div>`    
+              
+
+  }
+  output.innerHTML = build;
+  result.innerHTML = ""
 }
