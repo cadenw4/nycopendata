1f4fc2f64200fb273ac5ea817f109ccf4fcd2bed
cadenw4 <cadenw4@nycstudents.net>
2026-05-29 13:30:19 +0000

    filter wip2

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ async function init(){
 }
 */
 
-function reset(){
+function  display(){
   let output = document.getElementById("output");
   let result = document.getElementById("result");
   let build = "";
@@ -28,10 +28,25 @@ for(let i = 0; i < data.length; i+=1){
 if(sq.primary_fur_color == null){
   sq.primary_fur_color = ""
 }
+if(sq.age == null){
+  sq.age = ""
+}
+
+if(sq.specific_location == null){
+  sq.specific_location = ""
+}
+
+if(sq.other_activities == null){
+  sq.other_activities = ""
+}
+
     build += `<div class="fitted card">
   <h1>ID: ${sq.unique_squirrel_id} </h1> 
   <p>Coordinates: ${sq.x}, ${sq.y}</p>
-  <p>Fur color: </p> <p>${sq.primary_fur_color}</p>
+  <p>${sq.primary_fur_color}</p>
+  <h3>${sq.age}</h3>
+  <p>${sq.specific_location}</p>
+  <p>${sq.other_activities}</p>
               </div>`    
               
 
