8ae8211ac8545d911999dc372ba5d1135e0ba6e3
cadenw4 <cadenw4@nycstudents.net>
2026-06-03 13:02:49 +0000

    home

--- filterjs.js
+++ filterjs.js
@@ -5,39 +5,51 @@ async function init(){
   info = await fetch(link);
   data = await info.json();
   
+
   let output = document.getElementById("output");
   let build = "";
 
   for(let i = 0; i < data.length; i+=1){
-    for(let i = 0; i < data.length; i+=1){
     let sq = data[i];
-if(sq.primary_fur_color == null){
-  sq.primary_fur_color = ""
-}
-if(sq.age == null){
-  sq.age = ""
-}
 
-if(sq.specific_location == null){
-  sq.specific_location = ""
-}
+    if(sq.primary_fur_color == null){
+      sq.primary_fur_color = ""
+    }
+    if(sq.age == null){
+      sq.age = ""
+    }
 
-if(sq.other_activities == null){
-  sq.other_activities = ""
-}
+    if(sq.specific_location == null){
+      sq.specific_location = ""
+    }
 
-build += `<div class="fitted card">
-  <h1>ID: ${sq.unique_squirrel_id} </h1> 
-  <p>Coordinates: ${sq.x}, ${sq.y}</p>
-  <p>${sq.primary_fur_color}</p>
-  <h3>${sq.age}</h3>
-  <p>${sq.specific_location}</p>
-  <p>${sq.other_activities}</p>
+    if(sq.other_activities == null){
+      sq.other_activities = ""
+    }
+
+    build += `<div class="fitted card">
+                  <h1>ID: ${sq.unique_squirrel_id} </h1> 
+                  <p>Coordinates: ${sq.x}, ${sq.y}</p>
+                  <p>${sq.primary_fur_color}</p>
+                  <h3>${sq.age}</h3>
+                  <p>${sq.specific_location}</p>
+                  <p>${sq.other_activities}</p>
               </div>`    
-              
-
+            
   }
   output.innerHTML = build;
-  result.innerHTML = ""
+
 }
+
+
+
+function filterByAge(){
+  let output = document.getElementById("output");
+  let build = "";
+  result = document.getElementById("result")
+  let agefilter = document.getElementById("age-select")
+  if(agefilter == "juvenile"){
+    console.log("juvenile checked off")
+
+  }
 }
